398152eef9ca51a634a5d0d90ded06b3b078e650
Stephane Lee <stelee22@hotmail.com>
2013-05-14 14:28:56 -0400

    Gay

--- lab1/sum.vhd
+++ lab1/sum.vhd
@@ -8,11 +8,14 @@ entity sum is
 end sum;
 
 architecture main of sum is
-
-  -- insert your code here 
+	begin
+		o_sum <= i_a xor i_b xor i_cin;
 
 end architecture;
 
 -- question 1
-  -- insert answer here
+  -- In Precision RTL, we see a generated circuit schematic of the vhd code. 
+  -- The inputs are seen on the left (i_b, i_cin, i_a)
+  -- These inputs drive the two XOR gates
+  -- The output is displayed on the right of the schematic
 
