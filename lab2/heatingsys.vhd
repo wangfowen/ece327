93b8409df48ab3934414240ef6488a79f696d5e8
Stephane Lee <stelee22@hotmail.com>
2013-05-22 23:32:11 -0400

    faggg lab2 stuff

--- lab2/heatingsys.vhd
+++ lab2/heatingsys.vhd
@@ -24,13 +24,58 @@ entity heatingsys is                           -- finite state machine
 end heatingsys;
 
 architecture main of heatingsys is
-  signal state : heat_ty;   
+  signal state 	: heat_ty;
+	signal temp_diff 	: signed(7 downto 0);   
 begin
 
-  -- insert your vhdl code here
+	temp_diff <= i_des_temp - i_cur_temp;
+	o_heatmode <= state;
 
+	statemachine : process
+	begin	
+		wait until rising_edge(i_clock); 
+		if i_reset = '1' then
+			state <= off;
+		else 
+			case state is
+				when off =>
+					if temp_diff >= 5 then
+						state <= high;
+					elsif temp_diff >= 3 then
+						state <= low;
+					else 
+						state <= off;
+					end if;
+				
+				when low =>
+					if temp_diff >= 7 then
+						state <= high;
+					elsif temp_diff >= 2 then
+						state <= low;
+					else 
+						state <= off;
+					end if;
+				
+				when high =>
+					state <= off;
+					if temp_diff >= 3 then
+						state <= high;
+					else 
+						state <= low;
+					end if;
+
+				-- Invalid state should go to off for safety
+				when others =>
+					state <= off;
+			end case;
+		end if;
+	end process;		
 end main;
 
 -- question 1
-  --insert answer here
-
+	-- The total number of basic elements in our design is as follows:
+	-- 13 NOT gates
+	-- 9 AND gates
+	-- 13 OR gates
+	-- 2 1-bit flip-flops (which is actually just one 2-bit flip-flop)
+	-- 1 subtractor
