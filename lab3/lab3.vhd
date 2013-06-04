06f9973f49159190a62fe3c98d0b2447137679b6
Owen Wang <wangfowen@gmail.com>
2013-06-04 15:29:16 -0400

    yeeeeeeeeeeeee

--- lab3/lab3.vhd
+++ lab3/lab3.vhd
@@ -13,27 +13,37 @@ entity lab3 is
 end entity lab3;
 
 architecture main of lab3 is
+  signal count : unsigned(7 downto 0) := to_unsigned(0, 8);
+  signal calculation : signed(9 downto 0);
+  signal row_counter : unsigned(3 downto 0) := to_unsigned(0, 4);
+  signal column_counter : unsigned(3 downto 0) := to_unsigned(0, 4);
+
+   -- A function to rotate left (rol) a vector by n bits
+  function "rol" ( a : std_logic_vector; n : natural )
+  return std_logic_vector
+  is
+  begin
+      return std_logic_vector( unsigned(a) rol n );
+  end function;
 begin
+  calc : process
+  begin
+    wait until rising_edge(i_clock);
+  -- when push button 0 is pressed and before each matrix set, set reset to 1
 
- -- A function to rotate left (rol) a vector by n bits
- -- 
-function "rol" ( a : std_logic_vector; n : natural )
-return std_logic_vector
-is
-begin
-    return std_logic_vector( unsigned(a) rol n );
-end function;
+  -- if reset is 1:
+  -- clear matrix, set state to 000, counter to 0x00
 
--- when push button 0 is pressed and before each matrix set, set reset to 1
+  -- when i_valid is 1:
+    if unsigned(i_input) >= 10 then
+      count <= count + 1;
+    end if;
+  -- store data in matrix
+  -- if it's after row 2 column 0, also do calculation and increment count
+  -- put count on seven segment display
+  end process;
 
--- if reset is 1:
--- clear matrix, set state to 000, counter to 0x00
-
--- when i_valid is 1:
--- store data in matrix
--- if it's after row 2 column 0, also do calculation and increment count
--- put count on seven segment display
-
+  o_output <= std_logic_vector(count);
 end architecture main;
 
 -- Q1: number of flip flops and lookup tables?
