d7ee99c199cef8812b307f93dec7630e88bbbd62
Stephane Lee <stelee22@hotmail.com>
2013-06-10 00:48:11 -0400

    lots of fun stuff

--- lab3/lab3.vhd
+++ lab3/lab3.vhd
@@ -1,11 +1,18 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 package state_pkg is
   subtype state_ty is std_logic_vector(2 downto 0);
   constant S0 : state_ty := "001";
   constant S1 : state_ty := "010";
   constant S2 : state_ty := "100";
+
+  subtype mem_address is unsigned(3 downto 0);
+  type mem_address_vector is array(natural range <> ) of mem_address;
+  
+  subtype mem_data is unsigned(7 downto 0);
+  type mem_data_vector is array(natural range <>) of mem_data; 
 end state_pkg;
 
 library ieee;
@@ -28,24 +35,21 @@ architecture main of lab3 is
   signal calculation : signed(9 downto 0);
   signal row_counter : unsigned(3 downto 0);
   signal column_counter : unsigned(3 downto 0);
-  signal address1
-       , address2
-       , address3
-       : std_logic_vector(3 downto 0);
-  signal data1
-       , data2
-       , data3
-       , q1
-       , q2
-       , q3
-       : std_logic_vector(7 downto 0);
+  signal address : mem_address_vector(2 downto 0);
+  signal data
+       , q
+       : mem_data_vector(2 downto 0);
   signal a
        , b
        , c
-       : unsigned(9 downto 0);
+       : unsigned(7 downto 0);
   signal state : state_ty;
   signal row_index : state_ty;
+  signal row_index_and_i_valid : state_ty;
+
+  -- Optimization signals
   signal valid_sig_no_reset : std_logic;
+  signal goto_init : std_logic;
 
    -- A function to rotate left (rol) a vector by n bits
   function "rol" ( a : std_logic_vector; n : natural )
@@ -56,72 +60,46 @@ architecture main of lab3 is
   end function;
 
 begin
+  goto_init <= '1' when row_counter >= 15 and column_counter >= 15
+              else '0';
+  c <= unsigned(i_input);
 
-  valid_sig_no_reset <= not state(0) and i_valid;
-
-  mem1 : entity work.mem(main)
+  MEM_CPY: for I in 0 to 2 generate
+    row_index_and_i_valid(I) <= i_valid and row_index(I);
+    mem : entity work.mem(main)
     port map (
-      address => address1,
+      address => std_logic_vector(column_counter),
       clock => i_clock,
-      data => data1,
-      wren => i_valid,
-      q => q1
+      data => i_input,
+      wren => row_index_and_i_valid(I),
+      unsigned(q) => q(I)
     );
-  mem2 : entity work.mem(main)
-    port map (
-      address => address2,
-      clock => i_clock,
-      data => data2,
-      wren => i_valid,
-      q => q2
-    );
-  mem3 : entity work.mem(main)
-    port map (
-      address => address3,
-      clock => i_clock,
-      data => data3,
-      wren => i_valid,
-      q => q3
-    );
-
-  set_state : process
-  begin
-    wait until rising_edge(i_clock);
-    if (i_reset = '1' or (row_counter = 15 and column_counter = 15)) then
-      state <= S0;
-    end if;
-
-    if (i_valid = '1' and row_counter = 0 and column_counter = 0) then
-      -- state 1 - increment without calculations
-      state <= S1;
-    elsif (i_valid = '1' and row_counter = 2 and column_counter = 0) then
-      -- state 2 - start calculations
-      state <= S2;
-    end if;
-  end process;
+  end generate MEM_CPY;
 
   store_input : process
   begin
     wait until rising_edge(i_clock);
-  -- TODO: Test corner cases 255 + 255 and -255
-    if valid_sig_no_reset = '1' then
-      if (row_index(0) = '1') then
-        data1 <= i_input;
-        address1 <= std_logic_vector(column_counter);
-      elsif (row_index(1) = '1') then
-        data2 <= i_input;
-        address2 <= std_logic_vector(column_counter);
-      else
-        data3 <= i_input;
-        address3 <= std_logic_vector(column_counter);
-      end if;
+    
+    if i_valid = '1' then
+      -- Store input into memory
 
-      if (state(2) = '1') then
-        -- TODO: actulally do calculations correctly
-        a <= unsigned("00" & i_input);
-        b <= to_unsigned(100, 10);
-        c <= a;
-        calculation <= signed( a - b + c );
+      if (row_counter >= 2) then
+        -- TODO MAKE THIS COMBINATIONAL!!!
+        if (row_index(0) = '1') then
+          a <= q(1);
+          b <= q(2);
+        elsif (row_index(1) = '1') then
+          a <= q(2);
+          b <= q(0);
+        else
+          a <= q(0);
+          b <= q(1);
+        end if;
+
+        -- TODO: Test corner cases 255 + 255 and -255
+        calculation <= signed(("00" & a) - ("00" & b) + ("00" & c));
+
+        -- TODO add to count
       end if;
     end if;
   end process;
@@ -129,32 +107,41 @@ begin
   increment_counters : process
   begin
     wait until rising_edge(i_clock);
+    -- TODO reset state after end of matrix with goto_init and test optimizations
+    -- TODO check if making each counter/state machine its own process will help optimizations
 
-    if valid_sig_no_reset = '1' then
-      if (column_counter = 15) then
+    if (i_reset = '1') then     
+      column_counter <= to_unsigned(0, 4);
+      row_counter <= to_unsigned(0, 4);
+      count <= to_unsigned(0, 8);
+
+    elsif (i_valid = '1') then
+     
+      if (column_counter >= 15) then
         column_counter <= to_unsigned(0, 4);
         row_counter <= row_counter + 1;
-        row_index <= row_index rol 1;
       else
         column_counter <= column_counter + 1;
       end if;
-
-      -- TODO: Check if >= in lab manual
-      if (calculation >= 0) then
-        count <= count + 1;
-      end if;
-
-    -- We are in reset state
-    elsif (state(0) = '1') then
-      column_counter <= to_unsigned(0, 4);
-      row_counter <= to_unsigned(0, 4);
-      row_index <= S0;
-      count <= to_unsigned(0, 8);
-      calculation <= to_signed(0, 10);
+    
     end if;
   end process;
 
-  o_output <= q1;
+  rotate_row_index : process
+  begin
+    wait until rising_edge(i_clock);
+
+    if (i_reset = '1') then
+      row_index <= S0;
+
+    elsif (i_valid = '1') then  
+      if (column_counter >= 15) then
+        row_index <= row_index rol 1;
+      end if;
+    end if;
+  end process;
+
+  o_output <= std_logic_vector(q(0));
 end architecture main;
 
 -- Q1: number of flip flops and lookup tables?
@@ -165,4 +152,3 @@ end architecture main;
 
 -- Q3: source and destination signals of critical path?
 -- 
-
