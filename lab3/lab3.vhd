c54bf78f6a2be8304a30468afebcf074a5764df6
Stephane Lee <stelee22@hotmail.com>
2013-06-13 09:00:41 -0400

    based on piazza post

--- lab3/lab3.vhd
+++ lab3/lab3.vhd
@@ -123,11 +123,11 @@ end architecture main;
 
 -- Q1: number of flip flops and lookup tables?
 -- Within our lab3 cell there was 80 lookup tables and 28 flip flops.
--- This was found using uw-synth --chip lab3.uwp
+-- This was found using uw-synth --logic lab3.uwp
 
 -- Q2: maximum clock frequency?
 -- Our maximum clock frequency is 224 MHz for our lab3 cell
--- This was also found using uw-synth --chip lab3.uwp
+-- This was also found using uw-synth --logic lab3.uwp
 
 -- Q3: source and destination signals of critical path?
 -- The source of the critical path is reg_output/clk and the destination of the critical path is o_output
