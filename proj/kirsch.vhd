4ad8b223f85473e16f7726a417edfe25caf7ed65
Owen Wang <wangfowen@gmail.com>
2013-07-09 22:14:43 -0400

    Merge

--- proj/kirsch.vhd
+++ proj/kirsch.vhd
@@ -7,6 +7,7 @@ package state_pkg is
   constant S0 : state_ty := "001";
 
   subtype stage_state_ty is std_logic_vector(3 downto 0);
+  constant S0 : state_state_ty := "0000";
 
   subtype mem_data is unsigned(7 downto 0);
   type mem_data_vector is array(2 downto 0) of mem_data;
@@ -52,7 +53,7 @@ architecture main of kirsch is
   signal counter                  : unsigned(16 downto 0); -- 9 MSB is the row count, 8 LSB is the column count
   signal stage1_v             : stage_state_ty; -- todo
   signal stage2_v             : stage_state_ty; -- todo
-  
+
   signal mem_rd                   : mem_data_vector;
 
   --signal wr_data                  : mem_data_vector; -- done (hold last 2 values)
@@ -64,7 +65,7 @@ architecture main of kirsch is
   signal conv_vars                  : mem_data_crazy_vector;
   signal c1, d1                     : unsigned (7 downto 0);
   --signal a1, b1, c1,
-  --       h1, i1, d1, 
+  --       h1, i1, d1,
   --       g1, f1, e1,
   --       a2,
   --       h2,
@@ -101,25 +102,25 @@ begin
 
   --MEM_REDUN : for J in 0 to 2 generate
   MEM_CPY : for I in 0 to 2 generate
-    i_valid_and_mem_row_index(I) <= i_valid and mem_row_index(I); 
+    i_valid_and_mem_row_index(I) <= i_valid and mem_row_index(I);
     mem : entity work.mem(main)
     port map (
       address => std_logic_vector(counter(7 downto 0)),
       clock => i_clock,
       data => i_pixel,
       wren => i_valid_and_mem_row_index(I),
-      unsigned(q) => mem_rd(I) --mem_rd(I)(J) 
+      unsigned(q) => mem_rd(I) --mem_rd(I)(J)
     );
   end generate MEM_CPY;
   --end generate MEM_REDUN;
 
   -- todo: Replace this mux with tristate if possible
-  c1 <= (mem_rd(0) and (7 downto 0 => mem_row_index(2))) 
-    or (mem_rd(2) and (7 downto 0 => mem_row_index(1))) 
+  c1 <= (mem_rd(0) and (7 downto 0 => mem_row_index(2)))
+    or (mem_rd(2) and (7 downto 0 => mem_row_index(1)))
     or (mem_rd(1) and (7 downto 0 => mem_row_index(0)));
 
-  d1 <= (mem_rd(1) and (7 downto 0 => mem_row_index(2))) 
-    or (mem_rd(0) and (7 downto 0 => mem_row_index(1))) 
+  d1 <= (mem_rd(1) and (7 downto 0 => mem_row_index(2)))
+    or (mem_rd(0) and (7 downto 0 => mem_row_index(1)))
     or (mem_rd(2) and (7 downto 0 => mem_row_index(0)));
 
   increment_counters : process
@@ -143,7 +144,7 @@ begin
       mem_row_index <= mem_row_index rol 1;
     end if;
   end process;
- 
+
   latest_mem_rep_j : for J in 0 to 2 generate
     latest_mem_rep_i : for I in 0 to 2 generate
       latest_mem: process
@@ -161,7 +162,7 @@ begin
   latest_col: process
   begin
     wait until rising_edge(i_clock);
-    
+
     if (goto_init = '1') then
       conv_vars(3)(0) <= to_unsigned(0, 8);
       conv_vars(3)(1) <= to_unsigned(0, 8);
@@ -177,7 +178,6 @@ begin
       conv_vars(3)(0) <= c1;
       conv_vars(3)(1) <= d1;
       conv_vars(3)(2) <= unsigned(i_pixel);
-      
       --col_addr(0) <= col_addr(1);
       --col_addr(1) <= col_addr(2);
 
@@ -186,43 +186,64 @@ begin
     end if;
   end process;
 
-  set_mode : process
+--  set_mode : process
+--  begin
+--    wait until rising_edge(i_clock);
+--    if (goto_init = '1') then
+--      mode <= reset;
+--    end if;
+--    --TOOD: when set mode to busy? idle?
+--  end process;
+
+  stage_1 : process
   begin
     wait until rising_edge(i_clock);
     if (goto_init = '1') then
-      mode <= reset;
+      stage1_v <= S0;
+    elsif (i_valid = '1') then
+      stage1_v <= stage1_v rol 1;
+
     end if;
-    --TOOD: when set mode to busy? idle?
   end process;
 
-  detect_edge_stage_1 : process
+  stage_2 : process
   begin
     wait until rising_edge(i_clock);
     if (goto_init = '1') then
-    o_dir <= "000";
-    elsif (i_valid = '1' and counter(7 downto 0) >= 2 and counter(16 downto 8) >= 2) then -- Once we reach row 3 column 3, start doing convolution table stuff
-      
-    o_dir <= "111";
+      stage1_v <= S0;
+    elsif (stage1_v(3) = '1') then
+
     end if;
   end process;
 
-  --TODO: how pipeline so this happens at end of other stages..?
-  detect_edge_last_stage : process
-  begin
-    wait until rising_edge(i_clock);
-    o_valid <= '1';
-    --o_row <= std_logic_vector(row_count);
-    --TODO: output correct things
+--  detect_edge_stage_1 : process
+--  begin
+--    wait until rising_edge(i_clock);
+--    if (goto_init = '1') then
+--        o_dir <= "000";
+--    elsif (i_valid = '1' and counter(7 downto 0) >= 2 and counter(16 downto 8) >= 2) then -- Once we reach row 3 column 3, start doing convolution table stuff
+--
+--      o_dir <= "111";
+--    end if;
+--  end process;
 
-    if (goto_init = '1' or row_count = 255) then
-      row_count <= to_unsigned(0, 8);
-    else
-      row_count <= row_count + 1;
-    end if;
-  end process;
+--  --TODO: how pipeline so this happens at end of other stages..?
+--  detect_edge_last_stage : process
+--  begin
+--    wait until rising_edge(i_clock);
+--    o_valid <= '1';
+--    --o_row <= std_logic_vector(row_count);
+--    --TODO: output correct things
+--
+--    if (goto_init = '1' or row_count = 255) then
+--      row_count <= to_unsigned(0, 8);
+--    else
+--      row_count <= row_count + 1;
+--    end if;
+--  end process;
 
   o_mode <= mode;
-  
+
   -- For debugging
   o_edge <= '1' when counter(8) = '1' else '0';
   o_row <= std_logic_vector(counter(7 downto 0));
